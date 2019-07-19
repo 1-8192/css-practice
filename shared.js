01875dc66b49413218e0ad92d6dc92bbf02bca87
1-8192 <allegranzia@gmail.com>
2019-07-19 10:30:03 -0400

    added modal manipulating js

--- shared.js
+++ shared.js
@@ -1,10 +1,21 @@
 var backdrop = document.querySelector('.backdrop')
 var modal = document.querySelector('.modal')
 var selectPlanButton = document.querySelectorAll('.plan button')
+var noButton = document.querySelector(".modal__action--negative")
 
 for (let i = 0; i < selectPlanButton.length;i++) {
     selectPlanButton[i].addEventListener('click', function() {
-        modal.getElementsByClassName.display = "none"
-        backdrop.getElementsByClassName.display = "none";
+        modal.style.display = "none"
+        backdrop.style.display = "none"
     })
 }
+
+backdrop.addEventListener('click', function() {
+    modal.style.display = "none"
+    backdrop.style.display = "none"
+})
+
+noButton.addEventListener('click', function() {
+    modal.style.display = "none"
+    backdrop.style.display = "none"
+})
